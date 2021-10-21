900acb90436bf212d34dfaa664bbf6aa574706af
cjmaret <cjmaret@gmail.com>
2021-10-21 23:11:11 +0700

    feat: add prettier config, format users controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
-const User = require("../models/User");
-const NotFoundError = require("../errors/NotFoundError");
+const mongoose = require('mongoose');
+const User = require('../models/User');
+const NotFoundError = require('../errors/NotFoundError');
 
 module.exports.getCurrentUser = (req, res, next) => {
   User.findById(req.user._id).then((user) => {
     if (!user) {
-      throw new NotFoundError("User could not be found!");
+      throw new NotFoundError('User could not be found!');
     } else {
       return res.status(200).send({ user });
     }
