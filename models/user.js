19a76d4bc6d87cc9c2c256b25e12689ba8b09f74
cjmaret <cjmaret@gmail.com>
2021-10-22 13:57:20 +0700

    fix: remove userSchema name default string

--- models/user.js
+++ models/user.js
@@ -4,7 +4,6 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    default: 'Colin (winking face)',
     minlength: 2,
     maxlength: 30,
   },
